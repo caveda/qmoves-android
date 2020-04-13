45f72539107376e7550e5a45e205b612d72d122b
Victor Caveda <caveda@gmail.com>
2020-04-13 18:17:59 +0200

    Removed dead code

--- app/src/main/java/com/quoders/apps/qmoves/stops/StopsHostFragment.kt
+++ app/src/main/java/com/quoders/apps/qmoves/stops/StopsHostFragment.kt
@@ -66,26 +66,3 @@ class StopsHostAdapter(fragment: Fragment, val line : Line, val transport: Trans
         return fragment
     }
 }
-
-private const val ARG_OBJECT = "object"
-/*
-
-// Instances of this class are fragments representing a single
-// object in our collection.
-class DemoObjectFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_collection_object, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        arguments?.takeIf { it.containsKey(ARG_OBJECT) }?.apply {
-            val textView: TextView = view.findViewById(android.R.id.text1)
-            textView.text = getInt(ARG_OBJECT).toString()
-        }
-    }
-}*/
