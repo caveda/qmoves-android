dd4467311c93479e8e70639bf692560a8504376f
Victor Caveda <caveda@gmail.com>
2021-03-07 20:46:47 +0100

    Add fragment for details subviews

--- app/src/main/java/com/quoders/apps/qmoves/stopDetail/StopDetailFragment.kt
+++ app/src/main/java/com/quoders/apps/qmoves/stopDetail/StopDetailFragment.kt
@@ -15,9 +15,15 @@ import kotlinx.android.synthetic.main.fragment_stop_detail.*
 import kotlinx.android.synthetic.main.fragment_stops_host.*
 
 /**
- *  Fragment that hosts the stops pages: list, map.
+ *  Fragment that hosts the details of the stop: next transport, schedule, location.
  */
 class StopDetailFragment : Fragment() {
+
+    private val KEY_CURRENT_VIEW: String = "STOP_DETAIL_CURRENT_VIEW"
+    private var stopLocationFragment : Fragment? = null
+    private var stopNextFragment: Fragment? = null
+    private var stopScheduleFragment: Fragment? = null
+    private var currentVisibleViewId : Int = R.id.stopNextFragment
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,5 +34,82 @@ class StopDetailFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupBottomNavigationBar(view)
+        if (savedInstanceState != null) {
+            currentVisibleViewId = savedInstanceState.getInt(KEY_CURRENT_VIEW,R.id.stopNextFragment)
+        }
+        showViewFragment(currentVisibleViewId)
+    }
+
+    fun setupBottomNavigationBar(view: View) {
+        (stop_details_bottomnav_view as BottomNavigationView).setOnNavigationItemSelectedListener { menuItem ->
+            currentVisibleViewId = menuItem.itemId
+            showViewFragment(currentVisibleViewId)
+        }
+    }
+
+    private fun showViewFragment(viewId: Int): Boolean {
+        return when (viewId) {
+            R.id.stopNextFragment -> {
+                val fragment = createStopNextFragment()
+                openFragment(fragment)
+                true
+            }
+            R.id.stopScheduleFragment -> {
+                val fragment = createStopScheduleFragment()
+                openFragment(fragment)
+                true
+            }
+            R.id.stopLocationFragment -> {
+                val fragment = createStopLocationFragment()
+                openFragment(fragment)
+                true
+            }
+            else -> false
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(KEY_CURRENT_VIEW, currentVisibleViewId)
+        super.onSaveInstanceState(outState)
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = childFragmentManager.beginTransaction()
+        transaction.replace(R.id.stop_details_container, fragment)
+        transaction.commit()
+    }
+
+    private fun createStopNextFragment(): Fragment {
+        if (stopNextFragment == null) {
+            stopNextFragment = StopNextFragment()
+/*            stopLocationFragment?.arguments = Bundle().apply {
+                putParcelable("line", args.line)
+                putParcelable("transport", args.transport)
+            }*/
+        }
+        return stopNextFragment as Fragment
+    }
+
+    private fun createStopScheduleFragment(): Fragment {
+        if (stopScheduleFragment == null) {
+            stopScheduleFragment = StopScheduleFragment()
+/*            stopLocationFragment?.arguments = Bundle().apply {
+                putParcelable("line", args.line)
+                putParcelable("transport", args.transport)
+            }*/
+        }
+        return stopScheduleFragment as Fragment
+    }
+
+    private fun createStopLocationFragment(): Fragment {
+        if (stopLocationFragment == null) {
+            stopLocationFragment = StopLocationFragment()
+/*            stopLocationFragment?.arguments = Bundle().apply {
+                putParcelable("line", args.line)
+                putParcelable("transport", args.transport)
+            }*/
+        }
+        return stopLocationFragment as Fragment
     }
 }
---
+++ app/src/main/java/com/quoders/apps/qmoves/stopDetail/StopScheduleFragment.kt
@@ -0,0 +1,48 @@
+package com.quoders.apps.qmoves.stopDetail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.google.android.material.snackbar.Snackbar
+import com.quoders.apps.qmoves.EventObserver
+import com.quoders.apps.qmoves.R
+import com.quoders.apps.qmoves.data.Line
+import com.quoders.apps.qmoves.data.Transport
+import com.quoders.apps.qmoves.data.source.TransportRepositoryFactory
+import com.quoders.apps.qmoves.databinding.FragmentStopsBinding
+import com.quoders.apps.qmoves.favorites.FavoritesFragmentDirections
+import com.quoders.apps.qmoves.lines.LinesFragmentDirections
+import com.quoders.apps.qmoves.tools.setupSnackbar
+import com.quoders.apps.qmoves.tools.showSnackbar
+
+/**
+ *  Page that shows the list of stops of a line.
+ */
+class StopScheduleFragment : Fragment() {
+
+    private lateinit var line: Line
+    private lateinit var transport: Transport
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_stop_schedule, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+    }
+}
